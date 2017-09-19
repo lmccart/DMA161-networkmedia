7e67e75a96128078538b4805d5b1a583d07f197c
Lauren McCarthy <laurmccarthy@gmail.com>
2017-09-19 09:19:02 -0700

    schedg

---
+++ schedule/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html lang="en">
+<head>
+<?php include("../head.php"); ?>
+</head>
+<body>
+  <div class="container">
+    <?php include("../nav.php"); ?>
+
+    <div class="right">
+      <h2>SCHEDULE</h2>
+      <section>
+       <p>Coming soon!</p>
+      </section>
+    </div>
+  </div>
+</body>
+</html>
