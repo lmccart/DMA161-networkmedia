ec917bfd79f5cb8e4facb66accc440e357be2544
Lauren McCarthy <laurmccarthy@gmail.com>
2017-10-23 15:51:34 -0700

    weekly

--- weeklies/index.php
+++ weeklies/index.php
@@ -16,6 +16,15 @@
       <h2>WEEKLIES</h2>
       <p>Weeklies are due <b>before the start of class</b> on Thursday. Your index page should have a section with a link to each weekly. Weekly P/F grades are posted <a target="_blank" href="https://docs.google.com/spreadsheets/d/1KI-6mijcypeczlwRfitTRq2lbtJpA3hB1dGWXfkrv7U/edit?usp=sharing#gid=1091796845">here</a>, if you want to dispute a P/F you are responsible for notifying the TA within two weeks.</p>
       <section>
+        <h3 id="weekly4">WEEKLY 4 (DUE 10/26)</h3>
+        g̶̡l̵̸͟í̷͝t̕͠c͢h̛̕<br><br>
+        <ul>
+          <li>Read FastCo, <a href="https://www.fastcompany.com/3034296/inside-the-bizarre-phenomenon-known-as-glitch-art">Glitch Art</a>.</li>
+          <li>Find one glitch (either on a screen or in the physical world), take a photo or screenshot of it and post as your weekly response.</li>
+          <li>Create a new HTML page to hold your fourth weekly response, and link to it from your index page.</li>
+          <li><b>Not required.</b> If you are curious, more on glitch in Rosa Menkman's <a href="http://art.gcc.mass.edu/docs/art161/glitch.pdf">Glitch Studies Manifesto</a>.</li>
+        </ul>
+        
         <h3 id="weekly3">WEEKLY 3 (DUE 10/19)</h3>
         This week, as you design your portfolios or website, the readings are about internet aesthetics and alternatives. Consider these in contrast with Frank Chimero's essay from week 1.
         <ul>
@@ -39,15 +48,7 @@
           <li>First weekly response: Take an internet <a href="http://www.cddc.vt.edu/sionline/si/theory.html">dérive</a> through at least five links. Post the five links with a couple sentences that frame the journey by explaining the links relationship to each other.</li>
           <li>Place your index page and weekly page online and email a link to <a href='mailto:christinayglesias@gmail.com'>christinayglesias@gmail.com</a>.</li>
         </ul>
-        <!-- <h3 id="weekly4">WEEKLY 4 (DUE 10/26)</h3>
-        g̶̡l̵̸͟í̷͝t̕͠c͢h̛̕<br><br>
-        <ul>
-          <li>Read FastCo, <a href="https://www.fastcompany.com/3034296/inside-the-bizarre-phenomenon-known-as-glitch-art">Glitch Art</a>.</li>
-          <li>Find one glitch (either on a screen or in the physical world), take a photo or screenshot of it and post as your weekly response.</li>
-          <li>Create a new HTML page to hold your fourth weekly response, and link to it from your index page.</li>
-          <li><b>Not required.</b> If you are curious, more on glitch in Rosa Menkman's <a href="http://art.gcc.mass.edu/docs/art161/glitch.pdf">Glitch Studies Manifesto</a>.</li>
-        </ul>
-        
+        <!-- 
         <h3 id="weekly5">WEEKLY 5 (DUE 5/9)</h3>
         Networks of LA<br><br>
         <img src="network-guide.jpg">
