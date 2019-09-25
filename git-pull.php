94c6824f1ebebb11433ce6ae6e1832958cb6171d
Hirad Sab <hsab@users.noreply.github.com>
2019-09-25 14:22:28 -0700

    Update git-pull.php

--- git-pull.php
+++ git-pull.php
@@ -2,7 +2,7 @@
 echo 'pulling new';
 echo system('git reset --hard');
 echo system('git checkout -- .');
-echo system('git pull git@github.com:Lauren-McCarthy-Studio/DMA161-networkmedia.git\ master');
+echo system('git pull git@github.com:Lauren-McCarthy-Studio/DMA161-networkmedia.git master');
 
 $contents = array('date'=>$lib_date);
 file_put_contents('date.json', json_encode($contents));
