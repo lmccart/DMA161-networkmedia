a8492f8a9e4070371df19a76952ebd1e0a7b1b72
Lauren McCarthy <laurenleemccarthy@gmail.com>
2019-10-27 13:35:46 +0000

    ex

---
+++ notes/07_jQuery/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  
+//  $("#myText").animate({left: 300, top: 500}, 3000, "linear");
+  
+  
+  
+  $("#myText").animate({
+    left: 300,
+    top: 500,
+    fontSize: 60
+  }, 3000, "linear");
+  
+  
+});
