a7e17f5161d6f2b2187bf70488e5cfc337c23de1
Lauren Lee McCarthy <laurenleemccarthy@gmail.com>
2023-03-27 15:08:51 -0700

    s23

---
+++ _OLD/F19/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+  var hash = window.location.hash;
+  if (hash && $(hash).length) {
+    openSection(hash);
+  }
+
+  $.get('date.json', function(data) {
+    $('#updated_date').html(data.date);
+  });
+
+  $('#links .button').click(function() {
+    var selected = '#'+$(this).data('id');
+    openSection(selected);
+  });
+
+  $('#discussions h3').click(function() {
+    var selected = $(this).attr('id');
+    openSection('#discussions', selected);
+  });
+
+  $('#projects h3').click(function() {
+    var selected = $(this).attr('id');
+    openSection('#projects', selected);
+  });
+});
+
+function openSection(id, heading) {
+  if (open && open === heading) closeSection(heading);
+  else {
+    $('.block').hide();
+    if (heading) $('.toggle').hide();
+    $(id).show();
+
+    if (heading) {
+      $('#'+heading+'-content').show();    
+      if ($(id+' div.toggle:visible')[0]) {
+        open = $(id+' div.toggle:visible')[0].id.substring(0, 2);
+      }
+    }
+    window.location.hash = id.substring(1);
+    if (!heading) {
+      $(window).scrollTop(0);
+    }
+  }
+}
+
+function closeSection(heading) {
+  $('.toggle').hide();
+  open = null;
+}
+
