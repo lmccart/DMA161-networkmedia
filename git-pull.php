cf94d2cf7ec5ba1e1c89194c51ecd29378fc829f
Lauren McCarthy <lmccart@ucla.edu>
2023-03-28 22:22:04 -0700

    gp

--- git-pull.php
+++ git-pull.php
@@ -2,7 +2,7 @@
 echo 'pulling new';
 echo system('git reset --hard');
 echo system('git checkout -- .');
-echo system('git pull https://github.com/lmccart/DMA161-networkmedia.git main');
+echo system('git pull git@github.com:lmccart/DMA161-networkmedia.git main');
 
 // $contents = array('date'=>$lib_date);
 // file_put_contents('date.json', json_encode($contents));
